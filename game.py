154ebf859824b4f5a28a71a8b95c9f3818eb7a7e
Polardos <cyril.bazin77@gmail.com>
2024-03-25 00:51:09 +0100

    Rajout fin de jeux

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ class Game:
         self.game_on = False
 
         self.current_player = 'G'
+        self.winner = ' '
 
     def run(self):
         while True:
@@ -27,6 +28,7 @@ class Game:
 
     def menu_screen(self):
         self.grid.reset_grid()
+        self.winner = ' '
 
         self.screen.screen.fill((0, 0, 0))
         self.screen.print_text("Menu", (255, 255, 255), 0, -470)
@@ -52,11 +54,13 @@ class Game:
         if self.grid.check_win('G'):
             self.screen.print_text("WIN GREEN", (0, 255, 0), 0, -410)
             print("Les vert on win")
+            self.winner = 'G'
         elif self.grid.check_win('R'):
             self.screen.print_text("WIN RED", (255, 0, 0), 0, -410)
             print("Les Rouge on win")
+            self.winner = 'R'
 
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.screen.quit_screen()
@@ -66,11 +70,13 @@ class Game:
                     self.menu_on = True
                     self.game_on = False
                     return
-                elif pygame.K_KP1 <= event.key <= pygame.K_KP7:  # Vérifie si la touche est entre 1 et 7
+                elif pygame.K_KP1 <= event.key <= pygame.K_KP7 and self.winner == ' ':  # Vérifie si la touche est entre 1 et 7
                     col = event.key - pygame.K_KP1  # Calcule l'indice de colonne correspondant à la touche numérique
                     print(col + 1)  # Affiche le numéro de colonne
                     if self.grid.place_token(self.current_player, col):
                         self.switch_player()
+                    else:
+                        print("Impossible de placer le jeton")
 
     def switch_player(self):
         if self.current_player == 'G':
