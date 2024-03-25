03e503f0b9af7f3eb60a5a5f11dbdb6e66b592a7
Polardos <cyril.bazin77@gmail.com>
2024-03-25 01:45:06 +0100

    Ajout mode contre ia (coup random)

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 from screen import Screen
 from grid import Grid
@@ -13,7 +14,11 @@ class Game:
         self.menu_on = True
         self.game_on = False
 
+        self.alone_mode = None
+
         self.current_player = 'G'
+        self.current_player_is_ia = True
+
         self.winner = ' '
 
     def run(self):
@@ -32,24 +37,34 @@ class Game:
 
         self.screen.screen.fill((0, 0, 0))
         self.screen.print_text("Menu", (255, 255, 255), 0, -470)
-        self.screen.print_text("Press SPACE for play", (255, 255, 255), 0, -440)
+        self.screen.print_text("Press SPACE for play Alone", (255, 255, 255), 0, -440)
+        self.screen.print_text("Press TAB to play with others", (255, 255, 255), 0, -410)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.screen.quit_screen()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    print("Lancement du jeux")
+                    print("Lancement du jeu SEUL")
+                    self.alone_mode = True
+                    self.menu_on = False
+                    self.game_on = True
+                    return
+                elif event.key == pygame.K_TAB:
+                    print("Lancement du jeu SEUL")
+                    self.alone_mode = False
                     self.menu_on = False
                     self.game_on = True
                     return
 
     def game_screen(self):
-        self.screen.screen.fill((255, 255, 255))
-        self.screen.print_text("Game", (0, 0, 0), 0, -470)
-        self.screen.print_text("Press ESCAPE for menu screen", (0, 0, 0), 0, -440)
-
     
-        self.grid.print_grid(self.screen.screen)
+        if self.alone_mode:
+            self.screen.print_text("Alone Mode", (0, 0, 0), 0, -470)
+            self.mode_alone() #Mode seul 
+        else:
+            self.screen.print_text("Multiplayer Mode", (0, 0, 0), 0, -470)
+            self.mode_multiplayer()
+
 
         if self.grid.check_win('G'):
             self.screen.print_text("WIN GREEN", (0, 255, 0), 0, -410)
@@ -60,6 +75,29 @@ class Game:
             print("Les Rouge on win")
             self.winner = 'R'
 
+        
+        
+
+    def switch_player(self):
+        if self.current_player_is_ia:
+            self.current_player_is_ia = False
+        else:
+            self.current_player_is_ia = True
+
+        if self.current_player == 'G':
+            self.current_player = 'R'
+        else:
+            self.current_player = 'G'
+
+    def mode_multiplayer(self):
+        
+        if self.current_player == 'G':
+            self.screen.screen.fill((0, 255, 0))
+        else:
+            self.screen.screen.fill((255, 0, 0))
+        print(f"C'est le tour de {self.current_player}")
+        self.screen.print_text("Press ESCAPE for menu screen", (0, 0, 0), 0, -440)
+        self.grid.print_grid(self.screen.screen)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -78,8 +116,46 @@ class Game:
                     else:
                         print("Impossible de placer le jeton")
 
-    def switch_player(self):
+    def mode_alone(self):
         if self.current_player == 'G':
-            self.current_player = 'R'
+            self.screen.screen.fill((0, 255, 0))
         else:
-            self.current_player = 'G'
+            self.screen.screen.fill((255, 0, 0))
+        print(f"C'est le tour de {self.current_player}")
+        self.screen.print_text("Press ESCAPE for menu screen", (0, 0, 0), 0, -440)
+        self.grid.print_grid(self.screen.screen)
+
+        if self.current_player_is_ia:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.screen.quit_screen()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        print("Retour au menu")
+                        self.menu_on = True
+                        self.game_on = False
+                        return
+                    
+            #L'IA JOUE
+            if not self.grid.check_win('G') and not self.grid.check_win('R'):  # Vérifie si personne n'a gagné encore
+                col =random.randint(0,6)
+                if self.grid.place_token(self.current_player, col):
+                    self.switch_player()
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.screen.quit_screen()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        print("Retour au menu")
+                        self.menu_on = True
+                        self.game_on = False
+                        return
+                    elif pygame.K_KP1 <= event.key <= pygame.K_KP7 and self.winner == ' ':  # Vérifie si la touche est entre 1 et 7
+                        col = event.key - pygame.K_KP1  # Calcule l'indice de colonne correspondant à la touche numérique
+                        print(col + 1)  # Affiche le numéro de colonne
+                        if self.grid.place_token(self.current_player, col):
+                            self.switch_player()
+                        else:
+                            print("Impossible de placer le jeton")
+        
